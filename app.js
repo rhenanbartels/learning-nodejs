b3dc27fa208e7295fb4fad68cb7335b469ee8eed
Rhenan Bartels <rhenan.bartels@gmail.com>
2020-09-24 22:30:00 -0300

    Handle GET, POST and redirect

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const http = require('http');
 
 
 const server = http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.write("Hello there!")
-    response.end();
+    const method = request.method;
+    if(method === "GET" && request.url === "/"){
+        response.write("<html>");
+        response.write("<body>");
+        response.write(
+            "<form method='POST' action='/message'>"
+            + "<input type='text' name='message'>"
+            + "<button type='submit'>Send</button></form>"
+    );
+        return response.end();
+
+    } else if(method === "POST" && request.url === "/message"){
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
+
+        return response.end();
+    }
+
 });
 
 server.listen(3000);
